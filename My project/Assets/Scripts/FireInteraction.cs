76ced5ee7ce6658a6258b92f3f62ae3ec48a002e
Talha-Fakhar <talhafakhar506@gmail.com>
2023-08-01 10:40:11 -0700

    new project

--- My project/Assets/Scripts/FireInteraction.cs
+++ My project/Assets/Scripts/FireInteraction.cs
@@ -29,13 +29,27 @@ public class FireInteraction : MonoBehaviour
         }
     }
 
-    private void OnTriggerEnter(Collider other)
+    private void OnCollisionEnter(Collision collision)
     {
         // Check if the character collides with the fire and if the character is still alive
-        if (other.CompareTag("Character") && characterIsAlive)
+        if (collision.gameObject.CompareTag("Explodable") && characterIsAlive)
         {
+            // Destroy the character object
+            Destroy(collision.gameObject);
             // Set characterIsAlive to false to prevent repeated destruction
             characterIsAlive = false;
+
+            // Start the coroutine to extinguish the fire after fireDuration
+            StartCoroutine(ExtinguishFire());
         }
+    }
+
+    private System.Collections.IEnumerator ExtinguishFire()
+    {
+        yield return new WaitForSeconds(fireDuration);
+
+        // Stop emitting particles and destroy the fire game object
+        fireParticles.Stop();
+        Destroy(gameObject);
     }
 }
